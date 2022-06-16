447fc0b19930985d4ce8a5afa7689907d7e6f106
Sam Evans <evans.sam.t@gmail.com>
2022-06-16 09:41:59 -0600

    adds axios dependency and blog service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { BlogService } from './blog/blog.service';
 
 @Module({
   imports: [PostModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BlogService],
 })
 export class AppModule {}
