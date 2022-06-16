545deda71a2acfe70d883b20aecee60b690876ef
Sam Evans <evans.sam.t@gmail.com>
2022-06-16 09:23:21 -0600

    adds end-to-end tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { routeTwoResponse } from './data.spec';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,38 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/api (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api')
       .expect(200)
-      .expect('Hello World!');
+      .expect({ success: true });
+  });
+
+  it('/api/posts (GET) requires tag param', () => {
+    return request(app.getHttpServer())
+      .get('/api/posts?sortBy=likes&direction=desc')
+      .expect(400)
+      .expect({ error: 'Tags parameter is required' });
+  });
+
+  it('/api/posts (GET) does not allow invalid sorts', () => {
+    return request(app.getHttpServer())
+      .get('/api/posts?tags=history,tech&sortBy=something&direction=desc')
+      .expect(400)
+      .expect({ error: 'sortBy parameter is invalid' });
+  });
+
+  it('/api/posts (GET) does not allow invalid sort direction', () => {
+    return request(app.getHttpServer())
+      .get('/api/posts?tags=history,tech&sortBy=likes&direction=somethingelse')
+      .expect(400)
+      .expect({ error: 'direction parameter is invalid' });
+  });
+
+  it('/api/posts (GET) returns posts', () => {
+    return request(app.getHttpServer())
+      .get('/api/posts?tags=history,tech&sortBy=likes&direction=desc')
+      .expect(200)
+      .expect(routeTwoResponse);
   });
 });
