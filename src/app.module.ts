45008968ef34e2599d9344bde03e0841d4829e58
Sam Evans <evans.sam.t@gmail.com>
2022-06-16 14:47:54 -0600

    Adds auto caching app wide

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { BlogModule } from './blog/blog.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [PostModule, BlogModule],
+  imports: [PostModule, BlogModule, CacheModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
